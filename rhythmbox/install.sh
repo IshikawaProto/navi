ac9a688acc152c729669e47e780d32d21fd55980
Ishikawa Proto <ishikawa.proto@gmail.com>
2017-02-09 13:19:32 -0500

    Added rhythmbox

---
+++ rhythmbox/install.sh
@@ -0,0 +1,11 @@
+sudo apt install -y \
+    rhythmbox-plugin-alternative-toolbar
+    streamripper
+
+git clone https://github.com/fossfreedom/radio-browser -b rb3
+cd radio-browser
+sudo make install
+cd ..
+rm -rf radio-browser
+
+
