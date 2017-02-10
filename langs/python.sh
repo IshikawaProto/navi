9e80e3d3e7b6f1114b5167da77e0682a26735a95
Ishikawa Proto <ishikawa.proto@gmail.com>
2017-02-10 18:37:37 -0500

    Python installer

---
+++ langs/python.sh
@@ -0,0 +1,17 @@
+# System Libraries
+apt install -y \
+    libffi-dev \
+    libssl-dev \
+    libyaml-dev
+
+# System Python Env
+apt install -y \
+    python-dev \
+    python-pip \
+    python-virtualenv \
+    virtualenvwrapper
+
+# Install Python 3.6
+apt install -y \
+    python3.6 \
+    python3.6-dev
